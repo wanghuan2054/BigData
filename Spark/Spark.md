1a8affdbec51f6ab22c28d61170d91c0f09c9059
wanghuan2054 <499218471@qq.com>
2021-06-08 16:13:01 +0800

    pyspark SparkSQL&DataFrame

--- Spark/Spark.md
+++ Spark/Spark.md
@@ -54,6 +54,8 @@ http://node1:4040/
 
 ## Pyspark 安装
 
+https://spark.apache.org/docs/latest/api/python/getting_started/install.html
+
 ```shell
 [root@node1 data]# pip3 install pyspark
 ```
@@ -62,8 +64,6 @@ http://node1:4040/
 
 具体Spark与spark集成jar 位于 ：C:\Users\49921\Desktop\Spark-release-0.8.0\Spark-release-0.8.0\packaging\Spark-spark-bundle\target
 
-
-
 To build the Javadoc for all Java and Scala classes:
 
 ```
@@ -71,12 +71,28 @@ To build the Javadoc for all Java and Scala classes:
 mvn clean javadoc:aggregate -Pjavadocs
 ```
 
-### Build with Scala 2.12
+### PySpark 
 
-The default Scala version supported is 2.11. To build for Scala 2.12 version, build using `scala-2.12` profile
+User Guide
 
-```
-mvn clean package -DskipTests -Dscala-2.12
+https://spark.apache.org/docs/latest/sql-getting-started.html
+
+https://spark.apache.org/docs/latest/api/python/getting_started/quickstart.html
+
+```shell
+# 上传数据文件
+[root@node1 spark-3.0.0-hadoop3.2]# hadoop fs -put examples/src/main/resources/*.json /tmp
+# 进入pyspark shell
+[root@node1 spark-3.0.0-hadoop3.2]# bin/pyspark
+
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
 ```
 
 ### Build with Spark 3.0.0
@@ -88,113 +104,3 @@ Hadoop version supported is  2.7.3
 ```
 mvn clean package -DskipTests -Dspark3
 ```
-
-
-
-### Build with Sugon Cluster Version
-
-hadoop 3.1.1 
-zookeeper 3.4.6
-hive 3.1.0
-hbase 2.1.7 1.2.3
-spark 2.3.4
-kafka 2.4.1
-
-presto  ？
-
-修改源码中POM文件中的对应组件Version ， 尽量和上述版本一致
-
-```xml
-<!-- 修改如下组件版本即可 ，其余保持不动-->
-<properties>
-    <kafka.version>2.4.1</kafka.version>
-    <hadoop.version>3.1.1</hadoop.version>
-    <hive.version>3.1.0</hive.version>
-    <spark2.version>2.3.4</spark2.version>
-    <spark3.version>3.0.0</spark3.version>
-    <hbase.version>2.1.7</hbase.version>
-</properties>
-```
-再次执行打包命令
-
-```
-mvn clean package -DskipTests
-```
-
-### **mvn 报错**
-
-#### 1. Spark-hadoop-mr 缺少org.pentaho:pentaho-aggdesigner-algorithm:jar:5.1.5-jhyde 包
-
-```shell
-# 详细报错信息 
-[ERROR] Failed to execute goal on project Spark-hadoop-mr: Could not resolve dependencies for project org.apache.Spark:Spark-hadoop-mr:jar:0.9.0-SNAPSHOT: Could not find artifact org.pentaho:pentaho-aggdesigner-algorithm:jar:5.1.5-jhyde in maven-ali (http://maven.aliyun.com/nexus/content/groups/public//) -> [Help 1]
-```
-
-解决办法 ：
-
-1. 手动下载此jar包
-
-pentaho 中央仓库
-
-下载地址1： https://public.nexus.pentaho.org/repository/proxy-public-3rd-party-release/org/pentaho/pentaho-aggdesigner-algorithm/5.1.5-jhyde/pentaho-aggdesigner-algorithm-5.1.5-jhyde.jar
-
-MVN 公共仓库：
-
-下载地址2：https://mvnrepository.com/artifact/org.pentaho/pentaho-aggdesigner-algorithm/5.1.5-jhyde
-
-2. 将手动下载的jar移动到本地maven仓库地址中(推荐)
-
-   需要先在Spark 目录下执行一次mvn clean package -DskipTests ， 才能生存pentaho\pentaho-aggdesigner-algorithm\5.1.5-jhyde目录，之后再把jar包移进去
-
-   本地maven仓库地址：C:\Users\49921\.m2\repository\org\pentaho\pentaho-aggdesigner-algorithm\5.1.5-jhyde
-
-3. 或者手动install 本地jar包到本地仓库中
-
-   -Dfile 为本地jar路径
-
-   mvn install:install-file -DgroupId=org.pentaho -DartifactId=pentaho-aggdesigner-algorithm -Dversion=5.1.5-jhyde -Dpackaging=jar -Dfile=C:/users/49921/Desktop/pentaho-aggdesigner-algorithm-5.15-jhyde.jar
-
-   ###### mvn install:install-file将本地一个中央仓库没有的jar包，推到本地仓库
-
-   ###### 参考链接 ： https://www.cnblogs.com/daofree/p/12681855.html
-
-   
-
-#### 2. window环境下注释掉 Spark-integ-test和Spark-integ-test-bundle
-
-```shell
-# 报错信息
-[ERROR] Failed to execute goal org.codehaus.mojo:exec-maven-plugin:1.6.0:exec (Setup Spark_WS) on project Spark-integ-test: Command execution failed.: Cannot run program "\bin\bash" (in directory "C:\Users\49921\Desktop\Spark-release-0.8.0\Spark-release-0.8.0\Spark-integ-test"): CreateProcess error=2, 系统找不到指定的文件。 -> [Help 1]
-```
-
-### **解决办法**
-
-POM 文件注释报错的相关模块
-
-```xml
- <modules>
-    <module>Spark-common</module>
-    <module>Spark-cli</module>
-    <module>Spark-client</module>
-    <module>Spark-hadoop-mr</module>
-    <module>Spark-spark-datasource</module>
-    <module>Spark-timeline-service</module>
-    <module>Spark-utilities</module>
-    <module>Spark-sync</module>
-    <module>packaging/Spark-hadoop-mr-bundle</module>
-    <module>packaging/Spark-hive-sync-bundle</module>
-    <module>packaging/Spark-spark-bundle</module>
-    <module>packaging/Spark-presto-bundle</module>
-    <module>packaging/Spark-utilities-bundle</module>
-    <module>packaging/Spark-timeline-server-bundle</module>
-    <module>docker/hoodie/hadoop</module>
-    <!--  如下两个test模块注释掉
-	<module>Spark-integ-test</module>
-    <module>packaging/Spark-integ-test-bundle</module>
-	-->
-    <module>Spark-examples</module>
-    <module>Spark-flink</module>
-    <module>packaging/Spark-flink-bundle</module>
-  </modules>
-```
-
---
+++ Spark/pyspark/sugon/PySparkApi.py
@@ -0,0 +1,150 @@
+from pyspark.sql import SparkSession
+from datetime import datetime, date
+import pandas as pd
+from pyspark.sql import Row
+from pyspark.sql import Column
+from pyspark.sql.functions import upper
+from pyspark.sql.functions import pandas_udf
+from pyspark.sql.functions import expr
+
+# DataFrame Creation
+def createDataFrame():
+    # create a PySpark DataFrame from a list of rows
+    df = spark.createDataFrame([
+        Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
+        Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
+        Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
+    ])
+    # print(df.show())
+    # Create a PySpark DataFrame with an explicit schema
+    df1 = spark.createDataFrame([
+        (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 1)),
+        (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 2)),
+        (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 3))
+    ], schema='a long, b double, c string, d date, e timestamp')
+
+    # print(df1.show())
+    # Create a PySpark DataFrame from a pandas DataFrame
+    pandas_df = pd.DataFrame({
+        'a': [1, 2, 3],
+        'b': [2., 3., 4.],
+        'c': ['string1', 'string2', 'string3'],
+        'd': [date(2000, 1, 1), date(2000, 2, 1), date(2000, 3, 1)],
+        'e': [datetime(2000, 1, 1, 12, 0, 2), datetime(2000, 1, 2, 12, 0, 24), datetime(2000, 1, 3, 12, 0, 14)]
+    })
+    df2 = spark.createDataFrame(pandas_df)
+    # print(df2.show())
+
+    # Create a PySpark DataFrame from an RDD consisting of a list of tuples
+    rdd = spark.sparkContext.parallelize([
+        (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+        (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+        (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0))
+    ])
+    df3 = spark.createDataFrame(rdd, schema=['a', 'b', 'c', 'd', 'e'])
+
+    return df3
+
+# viewing the data
+def viewingData(df) :
+    spark.conf.set('spark.sql.repl.eagerEval.enabled', True)
+    # print(df.show())
+    # print(df.printSchema())
+    # print(df.show(2))
+    # print(df.columns)
+
+    # 列垂直显示
+    # print(df.show(1, vertical=True))
+    # 列汇总信息显示
+    # print(df.select("a", "b", "c").describe().show())
+
+    # collects all the data from executors to the driver side ， 如果数据集过大会造成OOM
+    # print(df.collect())
+
+    # 拉取固定条数的数据 , 避免OOM
+    # print(df.take(1))
+    # print(df.tail(1))
+    # 拉取df所有data 生成dataframe ， 会造成OOM
+    print(df.toPandas())
+
+# Selecting and Accessing Data
+def selectData(df) :
+    # 返回某一列的实例 ， 延迟计算并不会返回数据
+    # print(df.a)
+    # print(type(df.c) == type(upper(df.c)) == type(df.c.isNull()))
+
+    # 选择某一列
+    # print(df.select(df.c).show())
+
+    # 追加一列
+    print(df.withColumn('upper_c', upper(df.c)).show())
+    # 过滤行
+    print(df.filter(df.c == 'string2').show())
+
+# Grouping Data
+def plus_mean(pandas_df):
+    return pandas_df.assign(v1=pandas_df.v1 - pandas_df.v1.mean())
+def groupData() :
+    df = spark.createDataFrame([
+        ['red', 'banana', 1, 10], ['blue', 'banana', 2, 20], ['red', 'carrot', 3, 30],
+        ['blue', 'grape', 4, 40], ['red', 'carrot', 5, 50], ['black', 'carrot', 6, 60],
+        ['red', 'banana', 7, 70], ['red', 'grape', 8, 80]], schema=['color', 'fruit', 'v1', 'v2'])
+    # print(df.show())
+
+    # print(df.groupby('color').avg().show())
+    # print(df.groupby('fruit').max('v1').show())
+    print(df.groupby('color').applyInPandas(plus_mean, schema=df.schema).show())
+
+# Co-grouping and applying a function
+def asof_join(l, r):
+    return pd.merge_asof(l, r, on='time', by='id')
+
+def co_groupData() :
+    df1 = spark.createDataFrame(
+        [(20000101, 1, 1.0), (20000101, 2, 2.0), (20000102, 1, 3.0), (20000102, 2, 4.0)],
+        ('time', 'id', 'v1'))
+
+    df2 = spark.createDataFrame(
+        [(20000101, 1, 'x'), (20000101, 2, 'y')],
+        ('time', 'id', 'v2'))
+    print(df1.show())
+    print(df2.show())
+    print(df1.groupby('id').cogroup(df2.groupby('id')).applyInPandas(
+        asof_join, schema='time int, id int, v1 double, v2 string').show())
+
+# Getting Data in/out
+def csvImExport() :
+    df.write.csv('foo', header=True)
+    spark.read.csv('foo', header=True).show()
+
+def parquetImExport() :
+    df.write.parquet('bar.parquet')
+    spark.read.parquet('bar.parquet').show()
+def orcImExport() :
+    df.write.orc('zoo.orc')
+    spark.read.orc('zoo.orc').show()
+
+# Working with SQL
+def add_one(s):
+    return s + 1
+def sparkSQL():
+    df.createOrReplaceTempView("tableA")
+    spark.udf.register("add_one", add_one)
+    # print(spark.sql("SELECT * from tableA").show())
+    # print(spark.sql("SELECT count(*) from tableA").show())
+    # print(spark.sql("SELECT add_one(a) as a_plus_1 , * FROM tableA").show())
+    print(df.selectExpr('add_one(a)').alias('a_plus_1').show())
+    print(df.select(expr('count(*)') > 0).alias('CNT').show())
+if __name__ == '__main__':
+    spark = SparkSession.builder.getOrCreate()
+    # 生成 DataFrame
+    df = createDataFrame()
+    # 显示DataFrame
+    # viewingData(df)
+    # selectData(df)
+    # groupData()
+    # co_groupData()
+    # csvImExport()
+    # parquetImExport()
+    # orcImExport()
+    sparkSQL()
